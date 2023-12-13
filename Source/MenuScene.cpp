79e038cc0b2dcf20ccaf5ecfd781cfe727f87cd4
harontoo <haroncheruiyot254@outlook.com>
2023-12-13 12:17:41 +0300

    Added unimplemented function declarations and defination for the MenuScene class.

---
+++ Source/MenuScene.cpp
@@ -0,0 +1,25 @@
+#include "MenuScene.h"
+
+ax::Scene* createScene()
+{
+	auto scene = ax::Scene::create();
+
+	return scene;
+}
+
+bool MenuScene::init()
+{
+	return true;
+}
+
+ void MenuScene::startGame(ax::Ref* sender)
+{
+
+}
+
+
+void MenuScene::endGame(ax::Ref* sender)
+{
+
+}
+
---
+++ Source/MenuScene.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "axmol.h"
+
+class MenuScene : public ax::Layer
+{
+public:
+	static ax::Scene* createScene();
+	bool init();
+
+	static void startGame(ax::Ref* sender);
+	static void endGame(ax::Ref* sender);
+};
